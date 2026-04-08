7cdc3c9de6fbcb4662a4e1b5de37ae3fa9e9f8be
Sollace <sollacea@gmail.com>
2026-04-08 17:06:58 +0100

    Fixed fog color changing when standing in block-lit areas. Fixes #34

--- src/main/java/com/tamaized/voidfog/FogColor.java
+++ src/main/java/com/tamaized/voidfog/FogColor.java
@@ -9,9 +9,11 @@ import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.fog.FogData;
 import net.minecraft.client.renderer.fog.environment.FogEnvironment;
+import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.material.FogType;
 
 public class FogColor extends FogEnvironment {
@@ -37,7 +39,8 @@ public class FogColor extends FogEnvironment {
     @Override
     public float getModifiedDarkness(LivingEntity cameraEntity, float darkness, float tickProgress) {
         double prevBrightness = brightness;
-        float light = FogRenderer.getLight(cameraEntity);
+        BlockPos pos = BlockPos.containing(FogRenderer.getCorrectEntity(cameraEntity).getEyePosition());
+        float light = Math.max(0, cameraEntity.level().getBrightness(LightLayer.SKY, pos) - cameraEntity.level().getSkyDarken());
         brightness = light >= 1 ? darkness : (1 - light);
         return Math.max(darkness, (float)Mth.lerp(tickProgress / (brightness > prevBrightness ? 10 : 2), prevBrightness, brightness));
     }
